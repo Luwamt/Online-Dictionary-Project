0fa290d045b18f543bfdd4f1dc790cbcd6173c44
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2022-05-19 13:40:19 -0500

    updated

--- Dictionary.js
+++ Dictionary.js
@@ -34,26 +34,22 @@ console.log('Listenning the port on:'+port)
 
     let result = req.query.term;
     //const mysql=require('mysql');
- const   mysql = require('mysql2');
+ const   mysql = require('mysql');
 
     // Connection to DB Connected!
     const con=mysql.createConnection({
         host:"127.0.0.1",
         //port:3306,
-       user:"root",
-       password:"root",
+       user:"root1",
+       password:"password",
         database:"entries",
-        // insecureAuth: true,
-        // dialect: 'mysql',   
-        // multipleStatements: true,
     
     });
     con.connect();
-    //var sql = `SELECT word, definition FROM entries.entries where word '${word}'`;
 
-    // var sql ="SELECT * FROM entries.entries where word = '" + result +"'";
-    con.query("SELECT * FROM entries.entries where word = '" + result +"'",
-        function(err,data,fields){
+
+     var sql ="SELECT * FROM entries.entries where word = '" + result +"'";
+    con.query(sql,function(err,data,fields){
         if(err){ console.log(err);
         }else{
             console.log(`Database Connected`)  
